658d5eb05a9a7f657f792f365d00b0123714709c
debyamsingh2006-arch <debyamsingh2006@gmail.com>
2026-07-02 22:39:48 +0530

    Time: 6 ms (6.22%), Space: 8.4 MB (83.65%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -5,9 +5,12 @@ public:
         while(x != 0){
             rem = x%10;
             x = x/10;
-            if(ans > INT_MAX/10 || ans < INT_MIN/10){
+            if (ans > INT_MAX / 10 || (ans == INT_MAX / 10 && rem > 7)){
                 return 0;
             }
+            if (ans < INT_MIN / 10 || (ans == INT_MIN / 10 && rem < -8)){
+                return 0;
+            }    
             ans = ans*10 + rem;
         }
         return ans;
