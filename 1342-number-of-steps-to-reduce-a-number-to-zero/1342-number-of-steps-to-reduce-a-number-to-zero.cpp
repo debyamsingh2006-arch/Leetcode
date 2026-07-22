147ae4fdf3fae0889faf5c0bf318a788bf594b83
Debyam Singh <debyamsingh2006@gmail.com>
2026-07-22 22:34:17 +0530

    Time: 0 ms (100%), Space: 7.9 MB (80.15%) - LeetHub

--- 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.cpp
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.cpp
@@ -5,11 +5,10 @@ public:
         while(num>0){
             if(num%2==0){
                 num/=2;
-                count++;
             }else{
                 num-=1;
-                count++;
             }
+            count++;
         }
         return count;    
     }
