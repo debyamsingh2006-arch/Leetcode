64e4d3cdc7be5eaf21b2235a6a6a53569d08e52b
Debyam Singh <debyamsingh2006@gmail.com>
2026-08-14 15:05:49 +0530

    Time: 0 ms (100%), Space: 8.5 MB (87.75%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,12 +1,24 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        for (long long i=0;i<INT_MAX;i++){
-            if(x==i*i || ( x>i*i && x<(i+1)*(i+1) ) ){
-                return i;
+        if(x<2){
+            return x;
+        }
+        int ans,mid,start=1,end=x;
+        while(start<=end){
+            mid=start+(end-start)/2;
+            if(mid==x/mid){
+                ans=mid;
                 break;
             }
+            else if(mid<x/mid){
+                ans=mid;
+                start=mid+1;
+            }
+            else{
+                end=mid-1;
+            }
         }
-        return 0 ;
+        return ans;
     }
 };
